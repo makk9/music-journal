1e2318028d7e70cf27b111c96fad5624389e77e9
makk9 <makkenaa9@gmail.com>
2024-02-07 21:35:48 -0500

    Added testing for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import WebPlayback from './WebPlayback';
+import Login from './Login';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock child components - Webplayback and Login
+jest.mock('./WebPlayback', () => {
+  return {
+    __esModule: true,
+    default: () => <div>WebPlayback component</div>,
+  };
+});
+jest.mock('./Login', () => {
+  return {
+    __esModule: true,
+    default: () => <div>Login component</div>,
+  };
+});
+
+// Helper function to mock fetch response
+const mockFetch = (accessToken) => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ access_token: accessToken }),
+    }),
+  );
+};
+
+describe('App', () => {
+  it('renders Login component when there is no token', () => {
+    mockFetch(''); // Mock fetch to return empty token
+    render(<App />);
+    const loginElement = screen.getByText('Login component');
+    expect(loginElement).toBeInTheDocument();
+  });
+
+  it('renders WebPlayback component when token is retrieved', async () => {
+    mockFetch('fake-token'); // Mock fetch to return a fake token
+    render(<App />);
+    const webPlaybackElement = await screen.findByText('WebPlayback component');
+    expect(webPlaybackElement).toBeInTheDocument();
+  });
+
+  // Reset mocks after each test
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 });
